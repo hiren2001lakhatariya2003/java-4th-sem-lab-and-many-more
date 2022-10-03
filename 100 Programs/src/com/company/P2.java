40f94fc5c7bbdce3523e6a9c03304250121762e6
Hiren Lakhatariya <200540107061@darshan.ac.in>
2022-10-03 17:40:28 +0530

    add

--- 100 Programs/src/com/company/P2.java
+++ 100 Programs/src/com/company/P2.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 import java.lang.String;
 public class P2 {
     public static void main(String[] args) {
+        int flag = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string :");
-        String s = sc.nextLine();
-        for(int i=0;i<s.length();i++)
-        {
-            System.out.println(s[i]);
+        String str = sc.nextLine();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
         }
-
+        if (str.equals(rev)) {
+            System.out.println("palindrome");
+        } else {
+            System.out.println("not palindrome");
+        }
     }
-
 }
