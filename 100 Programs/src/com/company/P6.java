3445f2ff5b2f99c0e0d71256a30e57fff3d0eb47
Hiren Lakhatariya <200540107061@darshan.ac.in>
2022-10-06 10:41:57 +0530

    ad

--- 100 Programs/src/com/company/P6.java
+++ 100 Programs/src/com/company/P6.java
@@ -1,4 +1,27 @@
+/* Program(6):~
+Print a following pattern
+1
+1 2
+1 2 3
+1 2 3 4
+*/
 package com.company;
 
+import java.util.Scanner;
+
 public class P6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int number = sc.nextInt();
+
+        for (int i=1;i<=number;i++) { //Row
+            for (int j=1;j<=i;j++) { //Column
+                System.out.print(j+" ");
+            }
+            System.out.println();
+
+        }
+    }
 }
---
+++ 100 Programs/src/com/company/P7_iterative.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class P7_iterative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first = 0;
+        int second = 1;
+        System.out.println("Enter the number : ");
+        int number = sc.nextInt();
+        if (number == 0)
+            System.out.println("0");
+        else if (number == 1)
+            System.out.println("0 1");
+        else {
+            System.out.print("0 1 ");
+            for (int i = 2; i <= number; i++) {
+                int c = first + second;
+                System.out.print(c + " ");
+                first = second;
+                second = c;
+            }
+        }
+    }
+}
